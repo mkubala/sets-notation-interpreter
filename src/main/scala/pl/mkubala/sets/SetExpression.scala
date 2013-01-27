22a55de6eb614e523c564d37d882d6f49b601d51
Marcin Kubala <marcin@mkubala.pl>
2013-01-27 14:24:08 +0100

    change signature for SetBinOp case class

--- src/main/scala/pl/mkubala/sets/SetExpression.scala
+++ src/main/scala/pl/mkubala/sets/SetExpression.scala
@@ -2,4 +2,4 @@ package pl.mkubala.sets
 
 sealed trait SetExpression
 case class SetDef(elements: Set[Int]) extends SetExpression
-case class SetBinOp(opSymbol: String, leftBranch: SetExpression, rightBranch: SetExpression) extends SetExpression
+case class SetBinOp(leftBranch: SetExpression, opSymbol: String, rightBranch: SetExpression) extends SetExpression
