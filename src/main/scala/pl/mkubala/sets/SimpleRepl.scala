ab8006aa0e1c1a4a9a2eba74c90d66bbbccc671b
Marcin Kubala <marcin@mkubala.pl>
2013-02-09 01:34:58 +0100

    improve REPL environment

--- src/main/scala/pl/mkubala/sets/SimpleRepl.scala
+++ src/main/scala/pl/mkubala/sets/SimpleRepl.scala
@@ -1,9 +1,10 @@
 package pl.mkubala.sets
 
+import scala.annotation.tailrec
+
 import pl.mkubala.sets.evaluator.BasicSetOperations
 import pl.mkubala.sets.evaluator.SetNotationEvaluator
 import pl.mkubala.sets.parser.SetNotationParser
-import scala.annotation.tailrec
 
 object SimpleRepl extends App {
 
@@ -13,18 +14,21 @@ object SimpleRepl extends App {
   def readInput() {
     print("> ");
     readLine() match {
-      case "quit" | "exit" | "bye" =>
+      case "quit" | "exit" | "bye" => println("bye!")
       case expr => {
         try {
           println("= " + evaluator.eval(SetNotationParser.parse(expr)))
         } catch {
-          case ex: Exception => println("Error: " + ex)
+          case ex: Exception => println("Error: " + ex.getMessage)
         }
         readInput
       }
     }
   }
 
+  println("=== Sets Notation Interpreter - interactive shell ===")
+  println("Usage: Type expression which you want to parse and press Enter to see results")
+  println("If you want to exit, use one of the following expressions:\n 'bye', 'quit' or 'exit'")
+  println("Have fun!")
   readInput
-
 }
